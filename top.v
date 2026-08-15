8e19cc8f118bedc168bc22e5d94d27d2fb04a4c4
Tafseer_alig <150528497+Tafseer4169@users.noreply.github.com>
2026-08-15 16:41:38 +0530

    Update top.v

--- top.v
+++ top.v
@@ -17,7 +17,7 @@ wire sda, scl;
 wire ack_errm, ack_errs;
  
  
-i2c_master master (clk, rst, newd, addr, op, sda, scl, din, dout, busy, ack_errm , done);
+i2c_master master (clk, rst, addr,newd, op, sda, scl, din, dout, busy, ack_errm , done);
 i2c_Slave slave (scl, clk, rst, sda, ack_errs, );
  
 assign ack_err = ack_errs | ack_errm;
